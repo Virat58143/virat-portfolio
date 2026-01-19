d35e30edfe4accaae3486494a91b212a197728d8
Virat58143 <viratchoudhary741@gmail.com>
2026-01-19 14:51:22 +0530

    Fix assets path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/virat-portfolio/',   // 👈 repo name
+  base: '/virat-portfolio/',
+  build: {
+    outDir: 'docs',
+    assetsDir: 'assets',   // 👈 IMPORTANT
+    emptyOutDir: true
+  }
 })
